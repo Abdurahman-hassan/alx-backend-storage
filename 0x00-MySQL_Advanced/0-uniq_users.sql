eea3a7218ef4718294a0df838fce9ff7178daf35
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-14 03:38:16 +0300

    fix linting

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,3 @@
--- Creates users table.
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-);
+-- Script that creates a table users with requisite requirements
+-- Creates a table with unique id-nn,ai,pk; email-str(255) name-str(255)
+CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email CHAR(255) NOT NULL UNIQUE, name CHAR(255));
