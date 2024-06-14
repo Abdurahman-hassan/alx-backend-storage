894434715b2ff23a06692dea0cbc05ea3de84bd6
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-14 05:08:37 +0300

    update the script

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,5 @@
--- File: 3-glam_rock.sql
-SELECT
-    band_name,
-    IF(split IS NULL OR split = '', 2022 - formed, split - formed) AS lifespan
-FROM
-    metal_bands
-WHERE
-    main_style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+-- ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC
